fd550655851dc3e2ce0f7c028d5507e8660c0167
ArvindM15 <2416066@saec.ac.in>
2025-12-18 15:48:22 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 const navbar = document.getElementById("navbar");
-const themeBtn = document.getElementById("themeToggle");
-const body = document.body;
 
-// Scroll effect
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 80) {
+  if (window.scrollY > 50) {
     navbar.classList.add("scrolled");
   } else {
     navbar.classList.remove("scrolled");
   }
-});
-
-// Theme toggle
-themeBtn.addEventListener("click", () => {
-  body.classList.toggle("dark");
-
-  themeBtn.textContent = body.classList.contains("dark") ? "☀️" : "🌙";
 });
